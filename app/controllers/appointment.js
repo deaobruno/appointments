4e252ba9f3e6c27658f2f3a8c9d65ccc0f3b28d3
Bruno Oliveira <bruno.deao@gmail.com>
2021-05-07 23:22:22 -0300

    add controllers

---
+++ app/controllers/appointment.js
@@ -0,0 +1,76 @@
+import Appointment from '../models/appointment.js'
+
+class AppointmentController {
+  async findOne(req, res, next) {
+    await Appointment.findById(req.params.id, (err, result) => {
+      if (err) {
+        next(err)
+        return
+      }
+
+      if (!result) {
+        res.status(404).send({ error: 'Not found' })
+        return
+      }
+
+      res.status(200).send(result)
+    })
+  }
+
+  async findAll(req, res, next) {
+    await Appointment.find((err, result) => {
+      if (err) {
+        next(err)
+        return
+      }
+
+      res.status(200).send(result)
+    })
+  }
+
+  async create(req, res, next) {
+    await Appointment.create(req.body, (err, result) => {
+      if (err) {
+        next(err)
+        return
+      }
+
+      res.status(201).send({ success: 'Appointment created successfully.', data: result })
+    })
+  }
+
+  async update(req, res, next) {
+    await Appointment.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, (err, result) => {
+      if (err) {
+        next(err)
+        return
+      }
+
+      res.status(200).send({ success: 'Appointment updated successfully.', data: result })
+    })
+  }
+
+  async findByUser(req, res, next) {
+    await Appointment.find({ user: req.params.id }, (err, result) => {
+      if (err) {
+        next(err)
+        return
+      }
+
+      res.status(200).send(result)
+    })
+  }
+
+  async delete(req, res, next) {
+    await Appointment.deleteOne({ _id: req.params.id }, (err) => {
+      if (err) {
+        next(err)
+        return
+      }
+    
+      res.status(200).send({ success: 'Appointment deleted successfully.' })
+    })
+  }
+}
+
+export default new AppointmentController()
---
+++ app/controllers/user.js
@@ -0,0 +1,65 @@
+import User from '../models/user.js'
+
+class UserController {
+  async findOne(req, res, next) {
+    await User.findById(req.params.id, (err, result) => {
+      if (err) {
+        next(err)
+        return
+      }
+
+      if (!result) {
+        next(new Error('Not found'))
+        return
+      }
+
+      res.status(200).send(result)
+    })
+  }
+
+  async findAll(req, res, next) {
+    await User.find((err, result) => {
+      if (err) {
+        next(err)
+        return
+      }
+
+      res.status(200).send(result)
+    })
+  }
+
+  async create(req, res, next) {
+    await User.create(req.body, (err, user) => {
+      if (err) {
+        next(err)
+        return
+      }
+
+      res.status(201).send({ success: 'User created successfully.', data: user })
+    })
+  }
+
+  async update(req, res, next) {
+    await User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }, (err, result) => {
+      if (err) {
+        next(err)
+        return
+      }
+
+      res.status(200).send({ success: 'User updated successfully.', data: result })
+    })
+  }
+
+  async delete(req, res, next) {
+    await User.deleteOne({ _id: req.params.id }, (err) => {
+      if (err) {
+        next(err)
+        return
+      }
+    
+      res.status(200).send({ success: 'User deleted successfully.' })
+    })
+  }
+}
+
+export default new UserController()
