37137b462025d12fc303d812cc4be3aec6391ff7
Bruno Oliveira <bruno.deao@gmail.com>
2021-05-07 23:15:46 -0300

    appointment schema

---
+++ app/models/appointment.js
@@ -0,0 +1,33 @@
+import mongoose from '../../database/db.js'
+import User from '../models/user.js'
+      
+let timeRegex = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/
+
+let AppointmentSchema = mongoose.Schema({
+  date: {
+    type: Date,
+    required: [true, 'Missing attribute!'],
+  },
+  start: {
+    type: String,
+    required: [true, 'Missing attribute!'],
+    match: [timeRegex, 'Wrong format'],
+    minLength: [5, 'Wrong format'],
+    maxLength: [5, 'Wrong format'],
+  },
+  end: {
+    type: String,
+    required: [true, 'Missing attribute!'],
+    match: [timeRegex, 'Wrong format'],
+    minLength: [5, 'Wrong format'],
+    maxLength: [5, 'Wrong format'],
+  },
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId, ref: User,
+    required: [true, 'Missing attribute!'],
+  },
+})
+
+let AppointmentModel = mongoose.model('AppointmentModel', AppointmentSchema)
+
+export default AppointmentModel
