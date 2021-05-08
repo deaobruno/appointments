10089a77a3d037621480531651a8d8eae8172715
Bruno Oliveira <bruno.deao@gmail.com>
2021-05-07 23:28:19 -0300

    update server structure

---
+++ app.js
@@ -0,0 +1,5 @@
+import app from './server.js'
+
+const port = 8000
+
+app.listen(port, console.log('Server running'))
--- server.js
+++ server.js
@@ -3,13 +3,10 @@ import bodyParser from 'body-parser'
 import routes from './routes/index.js'
 
 const app = express()
-const port = 8000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use(routes)
 
-app.listen(port, () => {
-  console.log(`Server running at port ${ port }`)
-})
+export default app
