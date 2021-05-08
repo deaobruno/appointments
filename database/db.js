77d4290c070346beaeff06f742a175567ae79777
Bruno Oliveira <bruno.deao@gmail.com>
2021-05-07 23:13:47 -0300

    add database connection

---
+++ database/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+mongoose.connect(
+  'mongodb://localhost/appointments', 
+  { 
+    useFindAndModify: false,
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+  },
+  console.log('Connected to DB')
+)
+
+export default mongoose
